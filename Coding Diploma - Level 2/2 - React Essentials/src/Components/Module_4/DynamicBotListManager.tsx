daef6424d877fc71581df20c833803497430f976
Dan Thomas <36240712+dlt194@users.noreply.github.com>
2025-08-19 17:52:53 +0100

    Coding Diploma - Level 2 - React Essentials - Module 4.1, 4.1.1

---
+++ Coding Diploma - Level 2/2 - React Essentials/src/Components/Module_4/DynamicBotListManager.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+
+const DynamicBotManager = () => {
+  const [bots, setBots] = useState([
+    { id: 1, name: "Email Bot", status: "Active" },
+    { id: 2, name: "Data Bot", status: "Inactive" },
+  ]);
+  const [nextId, setNextId] = useState(bots.length + 1);
+  const [newBot, setNewBot] = useState({ id: 0, name: "", status: "" });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // Implement input change handler
+    setNewBot({
+      ...newBot,
+      id: nextId,
+      name: e.target.value,
+      status: "Inactive",
+    });
+  };
+
+  const addBotToList = () => {
+    // Implement add bot functionality
+    setBots((prevBots) => [
+      ...prevBots,
+      {
+        ...newBot,
+        id: nextId,
+        status: "Inactive",
+      },
+    ]);
+    setNextId((prev) => prev + 1);
+  };
+
+  const deleteBot = (id: number) => {
+    // Implement delete bot functionality
+    setBots(bots.filter((bot) => id !== bot.id));
+  };
+
+  return (
+    <div className="dynamic-bot-manager">
+      <h1 className="bold underline mb-4">Dynamic Bot Manager </h1>
+
+      {/* Add input fields for new bot */}
+      <input
+        type="text"
+        value={newBot.name}
+        onChange={(e) => handleInputChange(e)}
+        placeholder="Enter bot Name..."
+        className="m-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      />
+
+      {/* Add button to add new bot */}
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2"
+        onClick={addBotToList}
+      >
+        Add Bot
+      </button>
+
+      {/* Display list of bots */}
+      <ul className="flex overflow-auto">
+        {/* Map through bots and display each one */}
+        {bots.map(({ id, name, status }) => (
+          <li
+            key={id}
+            className="border border-gray-400 rounded m-2 min-w-[250px]"
+          >
+            <div className="flex flex-col mx-2">
+              <span className="m-1">ID: {id}</span>
+              <span className="m-1">Name: {name}</span>
+              <span className="m-1">Status: {status}</span>
+
+              <button
+                className="bg-red-500 hover:bg-red-700 text-white mx- p-1 m-1 rounded mb-4"
+                onClick={() => deleteBot(id)}
+              >
+                Delete Bot
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default DynamicBotManager;
--- Coding Diploma - Level 2/2 - React Essentials/src/Pages/Module_4.tsx
+++ Coding Diploma - Level 2/2 - React Essentials/src/Pages/Module_4.tsx
@@ -1,13 +1,51 @@
+import CodeExample from "../Components/CodeExample";
+import { CourseSectionComponent } from "../Components/CourseSectionComponent";
+
+import AddAndRemoveElements from "../Components/Module_4/AddAndRemoveElements.tsx?raw";
+import DynamicBotListManager from "../Components/Module_4/DynamicBotListManager.tsx?raw";
+
+import { default as AddAndRemoveElementsExample } from "../Components/Module_4/AddAndRemoveElements";
+import { default as DynamicBotListManagerExample } from "../Components/Module_4/DynamicBotListManager";
+
 function Module_4() {
   return (
     <>
-      <section className="bg-white rounded-lg shadow p-6 space-y-6">
-        <h2 className="text-xl font-semibold border-b pb-2"></h2>
-        <div className="prose max-w-none">
-          <p></p>
-        </div>
-      </section>
-      ;
+      <CourseSectionComponent
+        title="Add & Remove Elements"
+        children={
+          <div>
+            <CodeExample component={AddAndRemoveElements} />
+            <AddAndRemoveElementsExample />
+          </div>
+        }
+      />
+      <CourseSectionComponent
+        title="Practical Activity - Dynamic Bot List Manager with Add and Delete Functionality"
+        children={
+          <div>
+            <CodeExample component={DynamicBotListManager} />
+            <DynamicBotListManagerExample />
+          </div>
+        }
+      />
+      <CourseSectionComponent
+        title="Conditional Templates"
+        children={<div></div>}
+      />
+      <CourseSectionComponent
+        title="Practical Activity - Building a Modular Job Board Application"
+        children={<div></div>}
+      />
+      <CourseSectionComponent title="Props" children={<div></div>} />
+      <CourseSectionComponent
+        title="Practical Activity - Building a Modular Job Board with Props"
+        children={<div></div>}
+      />
+      <CourseSectionComponent title="Props Children" children={<div></div>} />
+      <CourseSectionComponent
+        title="Practical Activity - Building a Status Board with Dynamic Components"
+        children={<div></div>}
+      />
     </>
   );
 }

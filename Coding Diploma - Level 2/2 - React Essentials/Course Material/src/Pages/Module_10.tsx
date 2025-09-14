75b57ad589b994de54e0ea03ed2e369c5e152a44
Dan Thomas <36240712+dlt194@users.noreply.github.com>
2025-09-14 21:03:29 +0100

    Coding Diploma - Level 2 - React Essentials - Module 9/Module 10 Folder Setup

--- Coding Diploma - Level 2/2 - React Essentials/Course Material/src/Pages/Module_10.tsx
+++ Coding Diploma - Level 2/2 - React Essentials/Course Material/src/Pages/Module_10.tsx
@@ -1,13 +1,34 @@
+import { CourseSectionComponent } from "../Components/CourseSectionComponent";
+import CodeExample from "../Components/CodeExample";
+
 function Module_10() {
   return (
     <>
-      <section className="bg-white rounded-lg shadow p-6 space-y-6">
-        <h2 className="text-xl font-semibold border-b pb-2"></h2>
-        <div className="prose max-w-none">
-          <p></p>
-        </div>
-      </section>
-      ;
+      {/* 10.1 */}
+      <CourseSectionComponent
+        title="Introduction to API"
+        children={<div></div>}
+      />
+      {/* 10.2 */}
+      <CourseSectionComponent
+        title="API Key & Methods to Access API"
+        children={<div></div>}
+      />
+      {/* 10.3 */}
+      <CourseSectionComponent
+        title="Practice Exercise"
+        children={<div></div>}
+      />
+      {/* 10.4 */}
+      <CourseSectionComponent
+        title="Filtering & Sorting"
+        children={<div></div>}
+      />
+      {/* 10.5 */}
+      <CourseSectionComponent
+        title="Adding Reusable Components"
+        children={<div></div>}
+      />
     </>
   );
 }
--- Coding Diploma - Level 2/2 - React Essentials/Course Material/src/Pages/Module_9.tsx
+++ Coding Diploma - Level 2/2 - React Essentials/Course Material/src/Pages/Module_9.tsx
@@ -5,15 +5,24 @@ function Module_9() {
   return (
     <>
       {/* 9.1 */}
-      <CourseSectionComponent title="" children={<div></div>} />
+      <CourseSectionComponent title="Movie Review App" children={<div></div>} />
       {/* 9.1.1 */}
-      <CourseSectionComponent title="" children={<div></div>} />
+      <CourseSectionComponent
+        title="Practical Activity: Setting Up a Movie Review Application"
+        children={<div></div>}
+      />
       {/* 9.2 */}
-      <CourseSectionComponent title="" children={<div></div>} />
+      <CourseSectionComponent title="Adding Headers" children={<div></div>} />
       {/* 9.3 */}
-      <CourseSectionComponent title="" children={<div></div>} />
+      <CourseSectionComponent
+        title="Adding Components"
+        children={<div></div>}
+      />
       {/* 9.4 */}
-      <CourseSectionComponent title="" children={<div></div>} />
+      <CourseSectionComponent
+        title="Creating Movie Display Components"
+        children={<div></div>}
+      />
     </>
   );
 }

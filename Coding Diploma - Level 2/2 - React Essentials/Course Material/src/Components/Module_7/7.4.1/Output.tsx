740365881be91637c18d3a9939a80f717bdb59b7
Dan Thomas <36240712+dlt194@users.noreply.github.com>
2025-09-14 17:01:04 +0100

    Coding Diploma - Level 2 - React Essentials - 7.4.1

--- Coding Diploma - Level 2/2 - React Essentials/Course Material/src/Components/Module_7/7.4.1/Output.tsx
+++ Coding Diploma - Level 2/2 - React Essentials/Course Material/src/Components/Module_7/7.4.1/Output.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { calculateInvestmentResults } from "./utils/investment";
 
 interface OutputProps {
@@ -12,13 +12,18 @@ interface OutputProps {
 }
 
 const Output = ({ input, currency }: OutputProps) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   const resultData = useMemo(() => {
-    return calculateInvestmentResults({
+    setIsLoading(true);
+    const res = calculateInvestmentResults({
       initialInvestment: +input.initialInvestment,
       annualInvestment: +input.annualInvestment,
       expectedReturn: +input.expectedReturn,
       duration: +input.duration,
     });
+    setIsLoading(false);
+    return res;
   }, [input]);
 
   const currencySymbols: Record<OutputProps["currency"], string> = {
@@ -29,53 +34,58 @@ const Output = ({ input, currency }: OutputProps) => {
 
   return (
     <div className="px-2">
-      <table className="w-full border-collapse mt-8 text-[#66666E]">
-        <thead>
-          <tr>
-            <th className="border border-gray-700 bg-[#e6e6e9] p-4 text-right font-bold">
-              Year
-            </th>
-            <th className="border border-gray-700 bg-[#e6e6e9] p-4 text-right font-bold">
-              Investment Value
-            </th>
-            <th className="border border-gray-700 bg-[#e6e6e9] p-4 text-right font-bold">
-              Interest (Year)
-            </th>
-            <th className="border border-gray-700 bg-[#e6e6e9] p-4 text-right font-bold">
-              Total Interest
-            </th>
-            <th className="border border-gray-700 bg-[#e6e6e9] p-4 text-right font-bold">
-              Invested Capital
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {resultData.map((yearData, index) => (
-            <tr
-              key={index}
-              className={index % 2 === 0 ? "bg-[#9999a1]" : "bg-[#e6e6e9]"}
-            >
-              <td className="border border-gray-700 p-4 text-right">
-                {yearData.year}
-              </td>
-              <td className="border border-gray-700 p-4 text-right">
-                {currencySymbols[currency] +
-                  yearData.investmentValue.toFixed(2)}
-              </td>
-              <td className="border border-gray-700 p-4 text-right">
-                {currencySymbols[currency] + yearData.interest.toFixed(2)}
-              </td>
-              <td className="border border-gray-700 p-4 text-right">
-                {currencySymbols[currency] + yearData.totalInterest.toFixed(2)}
-              </td>
-              <td className="border border-gray-700 p-4 text-right">
-                {currencySymbols[currency] +
-                  yearData.investedCapital.toFixed(2)}
-              </td>
+      {isLoading ? (
+        <p className="text-gray-500 italic">Calculating results...</p>
+      ) : (
+        <table className="w-full border-collapse mt-8 text-[#66666E]">
+          <thead>
+            <tr>
+              <th className="border border-gray-700 bg-[#e6e6e9] p-4 text-right font-bold">
+                Year
+              </th>
+              <th className="border border-gray-700 bg-[#e6e6e9] p-4 text-right font-bold">
+                Investment Value
+              </th>
+              <th className="border border-gray-700 bg-[#e6e6e9] p-4 text-right font-bold">
+                Interest (Year)
+              </th>
+              <th className="border border-gray-700 bg-[#e6e6e9] p-4 text-right font-bold">
+                Total Interest
+              </th>
+              <th className="border border-gray-700 bg-[#e6e6e9] p-4 text-right font-bold">
+                Invested Capital
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {resultData.map((yearData, index) => (
+              <tr
+                key={index}
+                className={index % 2 === 0 ? "bg-[#9999a1]" : "bg-[#e6e6e9]"}
+              >
+                <td className="border border-gray-700 p-4 text-right">
+                  {yearData.year}
+                </td>
+                <td className="border border-gray-700 p-4 text-right">
+                  {currencySymbols[currency] +
+                    yearData.investmentValue.toFixed(2)}
+                </td>
+                <td className="border border-gray-700 p-4 text-right">
+                  {currencySymbols[currency] + yearData.interest.toFixed(2)}
+                </td>
+                <td className="border border-gray-700 p-4 text-right">
+                  {currencySymbols[currency] +
+                    yearData.totalInterest.toFixed(2)}
+                </td>
+                <td className="border border-gray-700 p-4 text-right">
+                  {currencySymbols[currency] +
+                    yearData.investedCapital.toFixed(2)}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };

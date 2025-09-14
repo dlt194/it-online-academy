c3167f9115a2f3bc8ddaa826e3a7eeb15ed6ea36
Dan Thomas <36240712+dlt194@users.noreply.github.com>
2025-09-14 21:44:20 +0100

    Coding Diploma - Level 2 - React Essentials - Module 9.1, 9.1.1

---
+++ Coding Diploma - Level 2/2 - React Essentials/Course Material/src/Components/Module_9/9.1/MovieReview.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MovieReview = () => {
+  return (
+    <div className="font-montserrat">
+      <nav>Nav Bar Content</nav>
+      <main>Main Content</main>
+    </div>
+  );
+};
+
+export default MovieReview;
--- Coding Diploma - Level 2/2 - React Essentials/Course Material/src/Pages/Module_9.tsx
+++ Coding Diploma - Level 2/2 - React Essentials/Course Material/src/Pages/Module_9.tsx
@@ -1,15 +1,36 @@
 import { CourseSectionComponent } from "../Components/CourseSectionComponent";
 import CodeExample from "../Components/CodeExample";
 
+import { default as MovieReview_9_1 } from "../Components/Module_9/9.1/MovieReview.tsx?raw";
+import { default as MovieReview_9_1_Example } from "../Components/Module_9/9.1/MovieReview";
+
+import { default as Header_9_1_1 } from "../Components/Module_9/9.1.1/components/Header.tsx?raw";
+import { default as MovieReview_9_1_1 } from "../Components/Module_9/9.1.1/MovieReview.tsx?raw";
+import { default as MovieReview_9_1_1_Example } from "../Components/Module_9/9.1.1/MovieReview";
+
 function Module_9() {
   return (
     <>
       {/* 9.1 */}
-      <CourseSectionComponent title="Movie Review App" children={<div></div>} />
+      <CourseSectionComponent
+        title="Movie Review App"
+        children={
+          <div>
+            <CodeExample component={MovieReview_9_1} />
+            <MovieReview_9_1_Example />
+          </div>
+        }
+      />
       {/* 9.1.1 */}
       <CourseSectionComponent
         title="Practical Activity: Setting Up a Movie Review Application"
-        children={<div></div>}
+        children={
+          <div>
+            <CodeExample component={Header_9_1_1} />
+            <CodeExample component={MovieReview_9_1_1} />
+            <MovieReview_9_1_1_Example />
+          </div>
+        }
       />
       {/* 9.2 */}
       <CourseSectionComponent title="Adding Headers" children={<div></div>} />
